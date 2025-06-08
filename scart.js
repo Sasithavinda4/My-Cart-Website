dd6b83abaf1baa18e67b1a1bd87749c4d6bcf6d0
Sasith Avinda Abeysundara <121947470+Sasithavinda4@users.noreply.github.com>
2025-06-08 16:33:40 +0530

    Add files via upload

---
+++ scart.js
@@ -0,0 +1,84 @@
+const product = [
+    {
+        id: 0,
+        image: 'iPhone-14.png',
+        title: 'iPhone 14',
+        price: 749,
+    },
+    {
+        id: 1,
+        image: 'Galaxy-S23-Ultra-Green.jpg',
+        title: 'Samsung Galaxy S23 Ultra',
+        price: 799,
+    },
+    {
+        id: 2,
+        image: 'Samsung-Galaxy-A73-5G-8GB-RAM-128GB-3.jpg',
+        title: 'Samsung Galaxy A73 5G',
+        price: 449,
+    },
+    {
+        id: 3,
+        image: 'iPhone-14-Pro-Max-Apple-Asia-Srilanka-Gold-1.png',
+        title: 'iPhone 14 Pro Max',
+        price: 1099,
+    }
+];
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image, title, price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>$ ${price}.00</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        `</div>
+        </div>`
+    )
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+}
+function delElement(a){
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart(){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "$ "+0+".00";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "$ "+total+".00";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+
+    
+}
